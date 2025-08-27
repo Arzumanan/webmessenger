307f01554fc0307c2f1f5fe5b073bee8c39cee09
muhammad <magomedov.mk80@gmail.com>
2025-08-27 14:33:47 +0300

    Создание структуры и добавление тестов по авторизации менеджера

--- pages/login_manager_page.py
+++ pages/login_manager_page.py
@@ -13,8 +13,6 @@ class LoginManagerPage(BasePage):
 
 
     PAGE_URL = Links.HOST
-    MANAGER_LOGIN_URL = Links.MANAGER_LOGIN_URL
-
     MANAGER_LOGIN_BUTTON = ("xpath", "//button[contains(text(), 'Менеджер')]")
     EMAIL_INPUT = ("xpath", "//input[@id = 'email']")
     PASSWORD_INPUT = ("xpath", "//input[@id = 'password']")
@@ -43,7 +41,7 @@ class LoginManagerPage(BasePage):
     def check_authorization(self, expected_result, expected_error_message):
         if expected_result:
             self.element_in_visible(self.DIALOGS_SIDEBAR).is_displayed()
-            assert self.browser.current_url != self.MANAGER_LOGIN_URL
+            assert self.browser.current_url == "https://testim.i2crm.ru/manager/login?limit=20&new_messages=false"
 
         else:
             error_locator = (By.XPATH, f"//*[text() = '{expected_error_message}']")
--- tests/test_login_manager.py
+++ tests/test_login_manager.py
@@ -3,24 +3,30 @@ import allure
 import pytest
 
 from pages.base_test import BaseTest
-from config.data import manager_email, invalid_manager_email, manager_password, invalid_manager_password
 
-@allure.suite("Авторизация менеджера")
+
+manager_email = "test-manager@test.ru"
+manager_password = "12345678"
+invalid_manager_email = "test-manager@test"
+invalid_manager_password = "1234"
+
+
+@allure.suite("Авторизация менеджера с разными данными")
 @pytest.mark.parametrize(
         "email, password, expected_result, expected_error_message",
         [
             pytest.param(manager_email, manager_password, True, None,
-                         id="authorization"),
-            pytest.param( invalid_manager_email, manager_password, False, "Пожалуйста, введите корректный email-адрес",
-                         id="ERROR Email"),
-            pytest.param(manager_email, invalid_manager_password, False, "Пароль должен содержать не менее 8 символов",
-                         id="ERROR password"),
+                         id="Успешная авторизация"),
+            pytest.param( invalid_manager_email, manager_password, False, "Значение «E-mail» не является правильным email адресом.",
+                         id="Ошибка Email"),
+            pytest.param(manager_email, invalid_manager_password, False, "Значение «Password» должно содержать минимум 8 символов.",
+                         id="Ошибка password"),
         ],
 )
 class TestManagerLogin(BaseTest):
 
 
-    @allure.title("Авторизация менеджера")
+    @allure.title("Авторизация успешная")
     def test_login_manager(self, email, password, expected_result, expected_error_message):
         self.base_page.open_host()
         self.login_manager_page.open_manager_login_page()
